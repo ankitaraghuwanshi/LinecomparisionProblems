467131b4d8ef0794ca84dcbc5388958572f101be
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-10 11:35:04 +0530

    UC2-LengthOfLine

--- LinecomparisionProblems/Program.cs
+++ LinecomparisionProblems/Program.cs
@@ -1,19 +1,15 @@
-﻿//UC1
-Console.WriteLine("Enter the co_ordinates x1 :");
-Console.WriteLine("Enter the co_ordinates x2 :");
-Console.WriteLine("Enter the co_ordinates y1 :");
-Console.WriteLine("Enter the co_ordinates y2 :");
+﻿//UC2
+Console.WriteLine("Enter the length of the lineone :");
+Console.WriteLine("Enter the length of the linetwo :");
 
-double x1 = double.Parse(Console.ReadLine());
-double x2 = double.Parse(Console.ReadLine());
-double y1 = double.Parse(Console.ReadLine());
-double y2 = double.Parse(Console.ReadLine());
+double lineone = double.Parse(Console.ReadLine());
+double linetwo = double.Parse(Console.ReadLine());
+if (lineone.Equals(linetwo))
+{
+    Console.WriteLine("lineone is eqal to linetwo");
 
-double lengthx = Math.Pow((x2 - x1), 2);
-double lengthy = Math.Pow((y2 - y1), 2);
-
-double sum = lengthx + lengthy;
-double lengthOfLine = (Math.Sqrt(sum));
-
-Console.WriteLine("Answer : " + lengthOfLine);
-
+}
+else
+{
+    Console.WriteLine("lineone is not equal to linetwo");
+}
