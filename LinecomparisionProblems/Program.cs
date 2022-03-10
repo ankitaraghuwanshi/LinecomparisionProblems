9462ced587df04ac9421095bd72d571d999faac8
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-10 11:13:26 +0530

    Welcome to linecompision

---
+++ LinecomparisionProblems/Program.cs
@@ -0,0 +1,19 @@
+﻿//UC1
+Console.WriteLine("Enter the co_ordinates x1 :");
+Console.WriteLine("Enter the co_ordinates x2 :");
+Console.WriteLine("Enter the co_ordinates y1 :");
+Console.WriteLine("Enter the co_ordinates y2 :");
+
+double x1 = double.Parse(Console.ReadLine());
+double x2 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
+
+double lengthx = Math.Pow((x2 - x1), 2);
+double lengthy = Math.Pow((y2 - y1), 2);
+
+double sum = lengthx + lengthy;
+double lengthOfLine = (Math.Sqrt(sum));
+
+Console.WriteLine("Answer : " + lengthOfLine);
+
